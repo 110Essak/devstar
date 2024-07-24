10885690a9c933291ea9daec6a0291c604f4b407
Cosmo784 <chaitanyachavhan07@gmail.com>
2024-07-24 13:18:27 +0530

    Main content

---
+++ src/lib/Carousel.svelte
@@ -0,0 +1,19 @@
+<script  lang="ts">
+    type DocumentType = {
+        title: string
+        src: string
+    }
+    export let documents: DocumentType[]
+  </script>
+  
+  
+  
+  <ul>
+    
+    {#each documents  as { title,src }}
+    <li>
+        <doc {title} {src} alt={title} />
+    </li>
+      
+    {/each}
+  </ul>
